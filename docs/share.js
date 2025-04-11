31832d7a1fbf722d7b3324ca1d87bd9a60d80fb0
Po-Yao Chen <alan4chen@gmail.com>
2025-04-11 11:56:03 +0100

    update readme and titles

--- docs/share.js
+++ docs/share.js
@@ -5,7 +5,7 @@ function shareToThreads() {
     if (!output) return alert("請先產生文宣再分享！");
 
     const base = "https://www.threads.net/intent/post?text=";
-    const signature = "\n\n 📢 下架不適任立委·罷免文宣產生器·2025一起守護民主 " + address;
+    const signature = "\n\n 📢 罷免文宣產生器·下架不適任立委·2025大罷免大成功" + address;
     const fullText = output + signature;
     const encoded = encodeURIComponent(fullText);
 
